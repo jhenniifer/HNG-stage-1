9c1bf456df941f9a539d8c4905e2731c614336f7
jhenniifer <chukwumakosisochukwu7gmail.com.gmail.com>
2025-02-06 20:59:40 +0100

    corrections

--- script.js
+++ script.js
@@ -7,13 +7,11 @@ const newGameButton = document.getElementById("newGameButton");
 
 const colors = ["rgb(255, 0, 0)", "rgb(255, 192, 203)", "rgb(152, 251, 152)", "rgb(175, 238, 238)", "rgb(250, 128, 114)", "rgb(154, 205, 50)"];
 let targetColor;
-
+gameStatus.textContent = '';
 
  function startGame(){
     targetColor = colors[Math.floor(Math.random() * colors.length)];
-    colorBox.style.backgroundColor = targetColor;
-    colorIndex = colors.indexOf(targetColor); 
-    gameStatus.textContent = 'Pick the correct color'
+    colorBox.style.backgroundColor = targetColor; 
     colorOptions.forEach(button => {
         button.disabled = false;
     });
@@ -30,7 +28,7 @@ let targetColor;
         scoreDisplay.textContent++;
         startGame();
     } else {
-        gameStatus.textContent = 'Ooopies! Try again 💔'
+        gameStatus.textContent = 'Oooppsies! Try again 💔'
         gameStatus.className = 'wrong';
          }
 }
